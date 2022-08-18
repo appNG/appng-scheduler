98b79660c577be861a605d158bebaa13e61bf0c6
Matthias Müller <Matthias.Mueller@aiticon.com>
2022-08-18 10:36:43 +0200

    SCHED-54 (workaround for APPNG-2448)

--- src/main/java/org/appng/application/scheduler/quartz/SchedulerFactoryBean.java
+++ src/main/java/org/appng/application/scheduler/quartz/SchedulerFactoryBean.java
@@ -15,9 +15,18 @@
  */
 package org.appng.application.scheduler.quartz;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Properties;
 
+import javax.sql.DataSource;
+
 import org.quartz.impl.jdbcjobstore.MSSQLDelegate;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * A custom {@link org.springframework.scheduling.quartz.SchedulerFactoryBean} that uses {@link DriverDelegateWrapper}.
@@ -25,6 +34,7 @@ import org.quartz.impl.jdbcjobstore.MSSQLDelegate;
  * "https://github.com/quartz-scheduler/quartz/blob/quartz-2.3.0/quartz-core/src/main/java/org/quartz/impl/jdbcjobstore/JobStoreSupport.java#L665">hard
  * coded check</a> on the name of the driver delegate class to determine the SQL lock statement.
  */
+@Slf4j
 public class SchedulerFactoryBean extends org.springframework.scheduling.quartz.SchedulerFactoryBean {
 
 	private static final String DRIVER_DELEGATE_CLASS = "org.quartz.jobStore.driverDelegateClass";
@@ -41,12 +51,50 @@ public class SchedulerFactoryBean extends org.springframework.scheduling.quartz.
 	public void setQuartzProperties(Properties quartzProperties) {
 		quartzProperties.put(DRIVER_DELEGATE_CLASS, DriverDelegateWrapper.class.getName());
 		quartzProperties.put(DRIVER_DELEGATE_INIT_STRING, "delegate=" + quartzDriverDelegate);
-		if (!quartzProperties.containsKey(SELECT_WITH_LOCK_SQL)
-				&& MSSQLDelegate.class.getName().equals(quartzDriverDelegate)) {
+		if (isMsSql() && !quartzProperties.containsKey(SELECT_WITH_LOCK_SQL)) {
 			quartzProperties.put(SELECT_WITH_LOCK_SQL, MSSSQL_LOCK_SQL);
 		}
-
+		log.info("Quartz properties: {}", quartzProperties);
 		super.setQuartzProperties(quartzProperties);
+	}
+
+	@Override
+	public void setDataSource(DataSource original) {
+		if (isMsSql()) {
+			// workaround for APPNG-2448
+			HikariConfig config = new HikariConfig();
+			HikariDataSource.class.cast(original).copyStateTo(config);
+			config.setAutoCommit(true);
+			config.setPoolName("appNG Quartz Hikari Pool");
+
+			String jdbcUrl = config.getJdbcUrl();
+			if (null != jdbcUrl) {
+				config.setJdbcUrl(getModifiedJdbcUrl(jdbcUrl));
+			} else {
+				jdbcUrl = config.getDataSourceProperties().getProperty("URL");
+				config.addDataSourceProperty("URL", getModifiedJdbcUrl(jdbcUrl));
+			}
+
+			HikariDataSource dataSource = new HikariDataSource(config);
+			try (Connection con = dataSource.getConnection()) {
+				log.info("Datasource: {} (autocommit: {})", dataSource, con.getAutoCommit());
+			} catch (SQLException e) {
+				log.error("Error testing connection", e);
+			}
+			super.setDataSource(dataSource);
+		} else {
+			super.setDataSource(original);
+		}
+	}
+
+	private String getModifiedJdbcUrl(String jdbcUrl) {
+		String modified = jdbcUrl + ";applicationName=appNG_Quartz_Scheduling";
+		log.info("Modified JDBC URL: {}", modified);
+		return modified;
+	}
+
+	private boolean isMsSql() {
+		return MSSQLDelegate.class.getName().equals(quartzDriverDelegate);
 	}
 
 }
